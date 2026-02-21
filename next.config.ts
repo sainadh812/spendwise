c0a2c89d2621d045cd5f5fc4cbf3d9682acca46e
Narendran <nkannan@gitlab.com>
2026-02-21 22:26:37 +0530

    Update next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  outputFileTracingIncludes: {
+    "/api/**/*": ["./src/generated/prisma/*.node"],
+    "/app/**/*": ["./src/generated/prisma/*.node"],
+  },
 };
 
 export default nextConfig;

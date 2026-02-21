1863d0b8cf6c8847780e39f0204d80b2f98fcd2b
Narendran <nkannan@gitlab.com>
2026-02-21 22:45:11 +0530

    Fix prism issue

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  outputFileTracingIncludes: {
-    "/api/**/*": ['./node_modules/.prisma/client/**/*', "./src/generated/prisma/*.node"],
-    "/app/**/*": ['./node_modules/.prisma/client/**/*', "./src/generated/prisma/*.node"],
-  },
 };
 
 export default nextConfig;
